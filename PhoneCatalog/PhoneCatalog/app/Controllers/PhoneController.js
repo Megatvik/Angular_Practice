9742c57d8443fa9cc87fc9353503de3d2a4332c7
Notebook <Matvik1103@gmail.com>
2016-06-29 12:45:57 +0300

    Add PhoneService. Rewrite PhoneController.

--- PhoneCatalog/PhoneCatalog/app/Controllers/PhoneController.js
+++ PhoneCatalog/PhoneCatalog/app/Controllers/PhoneController.js
@@ -4,42 +4,20 @@ angular
     .module('app')
     .controller('PhoneController', phoneController);
 
-function phoneController($http) {
-    var vm = this
+function phoneController(PhoneService) {
+    var vm = this;
 
-    vm.sortType = 'Name';
-    vm.sortReverse = false;
-    vm.searshName = '';
-    vm.showNewForm = false;
-    vm.refresh = function () {
-        // HTTP GET
-        // получение всех данных через GET запрос
-        $http.get("api/Phone/").then(function (responce) {
-            vm.data = responce.data
-        },
-    function (error) {
-        vm.message = "Error!"
-    });
-    }
+    vm.Get = function () {
+        PhoneService.GetPhones().then(function (data) { vm.data = data });
+    };
 
-    vm.create = function (item) {
-        // HTTP POST
-        // Отправка POST запроса для создания новой записи на сервере
-        $http.post("api/Phone/", item).then(function (item) {
-            //vm.data.push(item);
-            vm.refresh();
-        });
-    }
+    vm.Post = function (item) {
+        PhoneService.PostPhone(item).then(function (phone) { vm.Get(); });
+    };
 
-    vm.delete = function (item) {
-        // HTTP DELETE
-        // отправка DELETE запроса по адресу 
-        $http({
-            method: "DELETE",
-            url: "api/Phone/" + item.Id
-        }).success(function () {
-            vm.data.splice(vm.data.indexOf(item), 1);
-        });
-    }
-    vm.refresh();
+    vm.Delete = function (item) {
+        PhoneService.DelPhone(item).then(function (responce) { vm.data.splice(vm.data.indexOf(item), 1); });
+    };
+
+    vm.Get();
 }
---
+++ PhoneCatalog/PhoneCatalog/app/Services/PhoneService.js
@@ -0,0 +1,35 @@
+﻿
+angular
+    .module('app')
+    .service('PhoneService', PhoneService);
+
+function PhoneService($http) {
+
+    this.GetPhones = function () {
+        // HTTP GET
+        // получение всех данных через GET запрос
+        var promise = $http.get("api/Phone/").then(function (responce) {
+            return responce.data;
+        });
+        return promise;
+    };
+
+    this.PostPhone = function (phone) {
+        // HTTP POST
+        // Отправка POST запроса для создания новой записи на сервере
+        var promise = $http.post("api/Phone/", phone).then(function (phone) {
+            return phone.data;
+        });
+        return promise;
+    };
+
+    this.DelPhone = function (phone) {
+        // HTTP DELETE
+        // отправка DELETE запроса по адресу 
+        var promise = $http.delete("api/Phone/" + phone.Id).then(function () {
+
+        });
+        return promise;
+    };
+
+};
