ba88ad7c77842c59e1cd78e5a954f7a6906b800c
Megatvik <Matvik1103@gmai.com>
2016-07-26 12:46:44 +0300

    Login form. userController for login-exit.

---
+++ PhoneCatalog/PhoneCatalog/app/Controllers/userController.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+angular
+    .module('app')
+    .controller('userController', userController);
+
+function userController() {
+    var vm = this;
+    vm.isLogin = false;
+    vm.login = function (user) {
+
+        vm.isLogin = true;
+
+    };
+    vm.exit = function () {
+
+        vm.isLogin = false;
+
+    }
+}
---
+++ PhoneCatalog/PhoneCatalog/app/Directives/userControl.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+angular
+    .module('app')
+    .directive('userControl', userControl);
+
+function userControl() {
+    return {
+
+        restrict: 'E',
+        replace: true,
+        scope: true,
+
+        link: function ($scope, $element, $attrs) {
+
+        },
+
+        templateUrl: 'app/Views/common/userControl.html'
+    };
+};
